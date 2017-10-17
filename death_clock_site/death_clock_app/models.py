a3a77a979b9552dcc2b5ac473f33311ad179ed80
sklei2 <stk1151@rit.edu>
2017-10-17 13:14:48 -0400

    Also added UserInfo

--- death_clock_site/death_clock_app/models.py
+++ death_clock_site/death_clock_app/models.py
@@ -6,9 +6,31 @@ from django.contrib.auth.models import User
 
 
 """
-Extending the User model so that we can store information
-related to them in this class. 
-"""
-class Participant(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+Storing extra information in the User model for their 
+death information. This is information we'd get AFTER they
+complete the survey
 
+To access this information we grab the user object then go
+user_obj.DeathStats
+"""
+class DeathStats(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # date we think the user will live until
+    life_expectancy = models.DateField()
+
+
+"""
+Storing extra informatino in the User model for their
+general information. This is something that we could possibly
+get when we first create the profile.
+
+To access this information we grab the user object then go
+user_obj.UserInfo
+"""
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField()
+    sex = models.CharField(max_length=1, choices=(
+                                                   ('M', 'Male'),
+                                                   ('F','Female')
+                                                 ))
