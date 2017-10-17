658eba5fd0c073f7f30c013dd43ac94e427bda3d
sklei2 <stk1151@rit.edu>
2017-10-17 13:14:29 -0400

    In order to store extra information without extending the User class, we need to make a OneToOneField and then when we access the user object, we do .ClassName

--- death_clock_site/death_clock_app/models.py
+++ death_clock_site/death_clock_app/models.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class User(models.Model):
-    pass
+
+"""
+Extending the User model so that we can store information
+related to them in this class. 
+"""
+class Participant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
