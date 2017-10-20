80c21d6d415f9a84c66a4d3b316922f696a59668
sklei2 <stk1151@rit.edu>
2017-10-20 14:31:07 -0400

    made it so that we'd update our other models related to our user when we save our user. Also added it so that our urls go to our app's url file

--- death_clock_site/death_clock_app/models.py
+++ death_clock_site/death_clock_app/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 """
@@ -25,7 +27,7 @@ general information. This is something that we could possibly
 get when we first create the profile.
 
 To access this information we grab the user object then go
-user_obj.UserInfo
+user_obj.UserInf
 """
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -34,3 +36,18 @@ class UserInfo(models.Model):
                                                    ('M', 'Male'),
                                                    ('F','Female')
                                                  ))
+
+"""
+Hook up our extra information so that when we make a user we can have
+these fields.
+"""
+@receiver(post_save, sender=User)
+def create_user_prifle(sender, instance, created, **kwargs):
+    if created:
+        DeathStats.objects.create(user=instance)
+        UserInfo.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.deathStats.save()
+    instance.userInfo.save()
