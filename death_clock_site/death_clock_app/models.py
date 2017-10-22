2065d32dc380f965c3bca2863c60d4719c3a8e6c
Sean Klei <stk1151@rit.edu>
2017-10-22 15:41:43 -0400

    made it so that we have a concept of a UserProfile that will contain information like date of birth, sex, and our date of death. Also made it so that the information will show up on the admin page. Also included the migrations that will make it so it actually shows up.

--- death_clock_site/death_clock_app/models.py
+++ death_clock_site/death_clock_app/models.py
@@ -2,52 +2,48 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
 """
-Storing extra information in the User model for their 
-death information. This is information we'd get AFTER they
-complete the survey
-
-To access this information we grab the user object then go
-user_obj.DeathStats
-"""
-class DeathStats(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # date we think the user will live until
-    life_expectancy = models.DateField()
-
-
-"""
-Storing extra informatino in the User model for their
+Storing extra information in the User model for their
 general information. This is something that we could possibly
 get when we first create the profile.
 
 To access this information we grab the user object then go
 user_obj.UserInf
 """
-class UserInfo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='profile')
     dob = models.DateField()
     sex = models.CharField(max_length=1, choices=(
                                                    ('M', 'Male'),
-                                                   ('F','Female')
+                                                   ('F', 'Female')
                                                  ))
+    life_expectancy = models.DateField(null=True)
+
+    class Meta:
+        db_table = 'user_info'
+
 
 """
 Hook up our extra information so that when we make a user we can have
 these fields.
 """
-@receiver(post_save, sender=User)
-def create_user_prifle(sender, instance, created, **kwargs):
-    if created:
-        DeathStats.objects.create(user=instance)
-        UserInfo.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def save_user_profile(sender, instance, **kwargs):
-    instance.deathStats.save()
-    instance.userInfo.save()
+    instance.profile.save()
