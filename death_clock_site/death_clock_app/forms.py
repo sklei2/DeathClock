d4d7a3396a8bc867e636af69db7b25719cddb3b7
Sean Klei <stk1151@rit.edu>
2017-10-30 14:12:30 -0400

    made it so our clean method could potentially hold many errors, not just raise the first one

--- death_clock_site/death_clock_app/forms.py
+++ death_clock_site/death_clock_app/forms.py
@@ -28,8 +28,11 @@ class UserSignupForm(forms.Form):
 
     def clean(self):
         try:
+            errors = []
             if self.cleaned_data['pwd'] != self.cleaned_data['pwd_check']:
-                raise forms.ValidationError("Passwords do not match")
+                errors.append("Passwords do not match")
+            if any(errors):
+                raise forms.ValidationError(errors)
         except KeyError:
             pass
         return self.cleaned_data
